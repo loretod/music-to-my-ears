008759279b95a7c35855b472de9f853033678286
Loreto Dumitrescu <loretodalt@gmail.com>
2022-01-20 16:22:44 -0500

    0.0.4

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.7.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.3","target":"arcade","targetVersion":"1.7.24","repo":"loretod/music-to-my-ears"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.7.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.4","target":"arcade","targetVersion":"1.7.24","repo":"loretod/music-to-my-ears"}
 // total=1186349 new=7.18% cached=87.32% other=5.50%
 (function (ectx) {
 'use strict';
@@ -390,7 +390,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P61006(s) {
+function _main___P61189(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -774,13 +774,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P61006.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P61006.continuations = [  ]
+_main___P61189.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P61189.continuations = [  ]
 
-function _main___P61006_mk(s) {
+function _main___P61189_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P61006, depth: s.depth + 1,
+        parent: s, fn: _main___P61189, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -39506,5 +39506,5 @@ const sprites_BaseSpriteSayRenderer__C45856_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["mySprite___60884"])
 
-return _main___P61006
+return _main___P61189
 })
